869aa1e8798f9c6f7ebfb88664679aa652f59985
Francisco Javier Lindo <flindo10@gmail.com>
2017-05-31 14:12:03 +0200

    quitando puntos negros de los li

--- index.php
+++ index.php
@@ -3,15 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/css.css">
 </head>
 <body>
     <div class="contenedor">
         <ul>
-            <li>Inicio</li>
-            <li>Empresa</li>
-            <li>Tienda</li>
-            <li>Noticias</li>
-            <li>Contactar</li>
+            <li class="li-menu">Inicio</li>
+            <li class="li-menu">Empresa</li>
+            <li class="li-menu">Tienda</li>
+            <li class="li-menu">Noticias</li>
+            <li class="li-menu">Contacto</li>
         </ul>
     </div>
 </body>
